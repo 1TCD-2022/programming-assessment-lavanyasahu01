a30be10de4a0bb3de96d8153ef23b8f84465241c
lavanyasahu01 <103229998+lavanyasahu01@users.noreply.github.com>
2022-08-12 12:21:14 +1200

    Add files via upload

---
+++ Assesment V2.py
@@ -0,0 +1,98 @@
+
+"""
+Filename: Assesment DT1.7 DT1.8 
+Author: Lavanya Sahu 
+Date: 6/07/22
+Description: NZ QUIZ Question 
+"""
+#Questions:
+QandA = [
+    "1. What is the national animal of New Zealand?", "Kiwi",
+    "2. What is the capital city of New Zealand?", "Wellington",
+    "3.What is longest river in New Zealand?","Waikato River",
+    "4.What is the national sport of New Zealand?","Rugby",
+    "5.In which town can you find the hobbiton?","Matamata",
+    "6.Who was the first person to climb Mount Everest?", "Edmund Hillary",
+    "7.What dessert do Australians and New Zealanders both claim as their own?", "Pavlova",
+    "8. What is the name of the largest lake in New Zealand","Lake Taupo",
+    "9. What is the population of New Zealand? (in millions)", "5",
+    "10. Who is the current PM of New Zealand?","Jacinda Arden"
+]
+
+current = 0
+
+print ("Hii human being! Welcome to the NZ Quiz")
+print ("hope you are well preapred for this NZ quiz and know your knowledge about out beutiful country NZ")
+print ("You will get tested on 10 question based on New Zealand")
+
+
+question1 = input(QandA[current])
+if question1 == QandA[current+1]:
+  print ("Thats Correct")
+  current = current +2
+else:
+  print ("Sorry that was wrong,try again")
+
+
+question2 = input(QandA[current])
+if question2 == QandA[current+1]:
+  print ("That is Correct, welldone!")
+  current = current +2
+else:
+  print("sorry, that was wrong")
+
+question3 = input(QandA[current])
+if question3 == QandA[current+1]:
+  print ("That is Correct!")
+  current = current +2
+else:
+  print("sorry, that was wrong")
+
+question4 = input(QandA[current])
+if question4 == QandA[current+6]:
+  print ("That is Correct!")
+  current = current +2
+else:
+  print("sorry, that was wrong")
+
+question5 = input(QandA[current])
+if question5 == QandA[current+1]:
+  print ("That is Correct!")
+  current = current +2
+else:
+  print("sorry, that was wrong")
+
+question6 = input(QandA[current])
+if question6== QandA[current+1]:
+  print ("That is Correct!")
+  current = current +2
+else:
+  print("sorry, that was wrong")
+
+question7 = input(QandA[current])
+if question7== QandA[current+1]:
+  print ("That is Correct!")
+  current = current +2
+else:
+  print("sorry, that was wrong")
+
+question8 = input(QandA[current])
+if question8== QandA[current+1]:
+  print ("That is Correct!")
+  current = current +2
+else:
+  print("sorry, that was wrong")
+
+question9 = input(QandA[current])
+if question9== QandA[current+1]:
+  print ("That is Correct!")
+  current = current +2
+else:
+  print("sorry, that was wrong")
+
+question10 = input(QandA[current])
+if question10== QandA[current+1]:
+  print ("That is Correct!")
+  current = current +2
+else:
+  print("sorry, that was wrong")
